d3f8b4092a4f6faec48b0c7fa62d0aef73b5a5b8
EMASHQER <em-beauty@hotmail.com>
2012-06-02 16:56:22 +0300

    correct error in this page

--- login.php
+++ login.php
@@ -1,6 +1,4 @@
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
-    "http://www.w3.org/TR/html4/strict.dtd">
 
 <html>
     <head>
@@ -9,7 +7,7 @@
         <link type="text/css" rel="stylesheet" href="css/main.css" />
     </head>
     <body>
-                <form action="controllers/login.controller.php" method="POST" width="50" height="50">
+                <form action="controller/login.controller.php" method="POST" width="50" height="50">
                     <span>Please Enter your Login Details</span>
                     <label for="username">Username</label>
                     <input class="textfield" id="username" name="username" type="text" /><br />
@@ -20,8 +18,8 @@
 					<label><input type="radio" name="group1" value="adminstrator"/>adminstrator<label/>
 					</br>
 					<input name="dologin" type="submit" value="Login" />
-					
-                    <span class="loginerror"><?php echo ($_GET['error'] == 1) ? 'Invalid user name and password.' : ''; ?></span>
+                    <span class="loginerror"><?php echo ($_GET['invalid'] == 1) ? 'Invalid user name and password.' : ''; ?></span>
                 </form>
     </body>
 </html>
+
